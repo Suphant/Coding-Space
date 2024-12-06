002d2f51a1884136477c06da789860ac30d921ca
Suphant <suphanat.phaidong@gmail.com>
2024-12-06 14:42:37 +0700

    Add files via upload

---
+++ Python/conditions/odd_even.py
@@ -0,0 +1,4 @@
+num = 10
+rem = num % 2
+if rem == 0:
+    print( num, "Even")
---
+++ Python/exercise_3.py
@@ -0,0 +1,7 @@
+bonus = int(input('Input the amount of bonus money : '))
+
+mother = (30/bonus)*100 #0.3
+
+child = (bonus - mother) #0.7
+
+print('So amount each child receives is', child)
